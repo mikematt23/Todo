3c7b97a1b544c5ec34851938174d313fee0e1fa7
Michael MAtteis <michaelcmatteis@gmail.com>
2020-08-09 12:31:26 -0500

    finishing todlist

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import TodoItem from './Todoitem'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends Component {
+  constructor(props){
+    super(props)
+      this.state = {
+        isClicked : false,
+        text: '',
+        todos: []
+      }
+  }
+
+ changeState = ()=>{
+   this.setState({
+     todos: [...this.state.todos,this.state.text],
+     text:''
+   })
+ }
+ onChange =(e)=>{
+   this.setState({
+     text: e.target.value
+   })
+   console.log(this.state.todos)
+ }
+
+ delete = (index)=>{
+   let todoList = [...this.state.todos]
+   todoList.splice(index,1)
+   this.setState({
+     todos : todoList
+   })
+ }
+  
+  render(){
+    return (
+      <div className="App">
+        <input  value ={this.state.text} onChange = {this.onChange}/>
+        <button onClick = {this.changeState}>False</button>
+         <ul>
+           {this.state.todos.map((todo,index)=>{
+             return(
+               <TodoItem key ={index} delete = {this.delete} item = {todo} ></TodoItem>
+             )
+           })}
+         </ul>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
+
+
